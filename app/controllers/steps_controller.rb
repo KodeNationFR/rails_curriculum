34c07b64971abdfb25c4a3dd5b2dd5bfb2c02df4
kodenationfr <kodenation.co@gmail.com>
2019-07-25 17:09:52 +0200

    perf(app): delete json and generator

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -2,13 +2,11 @@ class StepsController < ApplicationController
   before_action :set_step, only: [:show, :edit, :update, :destroy]
 
   # GET /steps
-  # GET /steps.json
   def index
     @steps = Step.all
   end
 
   # GET /steps/1
-  # GET /steps/1.json
   def show
     redirect_to steps_path if @step.access === false
   end
@@ -23,43 +21,28 @@ class StepsController < ApplicationController
   end
 
   # POST /steps
-  # POST /steps.json
   def create
     @step = Step.new(step_params)
-
-    respond_to do |format|
-      if @step.save
-        format.html { redirect_to @step, notice: 'Step was successfully created.' }
-        format.json { render :show, status: :created, location: @step }
-      else
-        format.html { render :new }
-        format.json { render json: @step.errors, status: :unprocessable_entity }
-      end
+    if @step.save
+      redirect_to @step, notice: 'Step was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /steps/1
-  # PATCH/PUT /steps/1.json
   def update
-    respond_to do |format|
-      if @step.update(step_params)
-        format.html { redirect_to @step, notice: 'Step was successfully updated.' }
-        format.json { render :show, status: :ok, location: @step }
-      else
-        format.html { render :edit }
-        format.json { render json: @step.errors, status: :unprocessable_entity }
-      end
+    if @step.update(step_params)
+      redirect_to @step, notice: 'Step was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /steps/1
-  # DELETE /steps/1.json
   def destroy
     @step.destroy
-    respond_to do |format|
-      format.html { redirect_to steps_url, notice: 'Step was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to steps_url, notice: 'Step was successfully destroyed.'
   end
 
   private
